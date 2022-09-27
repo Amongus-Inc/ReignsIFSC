5764015b7edd97730109e16121c4c023c29f9a80
ArthurSchSilva-Schneider <arthur.ss06@aluno.ifsc.edu.br>
2022-09-27 20:24:35 +0000

    mundaças no bot

---
+++ botprofessor.py
@@ -0,0 +1,79 @@
+from definições import estados, partidas, status_dos_jogadores
+import discord
+from discord.ext import commands
+from random import choice
+import random
+from re import fullmatch
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix='')
+
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+@bot.event
+async def on_message(msg):
+    autor = msg.author.id
+    if autor == bot.application_id:
+        return
+
+    if autor not in partidas:
+        partidas[autor] = 0
+        status_dos_jogadores[autor] = {
+            'Sanidade': 50,
+            'Popularidade': 50,
+            'Notas': 50,
+            'Inteligência': 50
+        }
+
+
+    estado_do_jogador = estados[partidas[autor]]
+    status_sanidade = status_dos_jogadores[autor]['Sanidade']
+    status_popularidade = status_dos_jogadores[autor]['Popularidade']
+    status_notas = status_dos_jogadores[autor]['Notas']
+    status_inteligencia = status_dos_jogadores[autor]['Inteligência']
+
+    for key, value in estado_do_jogador['positivo_proximos_estados'].items():
+
+        if fullmatch(key, msg.content):
+            
+            await msg.channel.send(estado_do_jogador['frases_positivas'])
+            status_sanidade = status_sanidade + estado_do_jogador['positivos_sanidade']
+
+            await msg.channel.send(status_dos_jogadores[autor])
+            partidas[autor] = value
+            if estados[partidas[autor]]['2_estapas'] == 1:
+                estado_do_jogador = estados[partidas[autor]]
+            else:
+                partidas[autor] = random.randint(0, 3)
+                flip = random.randint(16, 17)#fazer ficar 1 à 25
+                if flip == 17:
+                    partidas[autor] = random.randint(2048, 2051)
+            estado_do_jogador = estados[partidas[autor]]
+            await msg.channel.send(partidas[autor])
+            await msg.channel.send(choice(estado_do_jogador['frases']))
+            return
+    for key, value in estado_do_jogador['negativa_proximos_estados'].items():
+        if fullmatch(key, msg.content):
+            await msg.channel.send(estado_do_jogador['frases_negativas'])
+            await msg.channel.send(status_dos_jogadores[autor])
+            partidas[autor] = value
+            partidas[autor] = random.randint(2, 3)
+            estado_do_jogador = estados[partidas[autor]]
+            await msg.channel.send(partidas[autor])
+            await msg.channel.send(choice(estado_do_jogador['frases']))
+            return
+    if partidas[autor] == 0:
+        await msg.channel.send(choice(estado_do_jogador['frases']))
+    else:
+        await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
+
+
+bot.run(getenv('DISCORD_TOKEN'))
---
+++ definições.py
@@ -0,0 +1,186 @@
+# Dicionário com as definições da máquina de estados do jogo.
+# As opções dos jogadores são definidas como expressões regulares.
+estados = {
+    0: {
+        'frases': ['Ricardito quer parte do teu lanche, você dá\? estado0'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },
+    1: {
+        'frases': ['estado1'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },
+    2: {
+        'frases': ['estado2'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },
+    3: {
+        'frases': ['estado3'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+
+    },
+    7: {
+        'frases': ['estado7'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+        
+    },
+    2048: {
+        'frases': ['estado2048'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },
+    2049: {
+        'frases': ['estado2049'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },    
+    2050: {
+        'frases': ['estado2050'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    },    
+    2051: {
+        'frases': ['estado2051'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'nagativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '[sS](i)+m': 7},
+        'negativa_proximos_estados': {
+            '[nN][aã]+o': 2
+        },
+        '2_estapas': 0
+    }
+}
+
+# Dicionário com os estados correntes de cada jogador.
+partidas = {}
+status_dos_jogadores = {}
+
+
+#cenarios mormais 0-1023
+#cenarios de continuação 1024-2047
+#cenarios historias 2048-3000
