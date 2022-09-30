e315e892ba9ba26b177a606a437b775a7965803f
ArthurSchSilva-Schneider <arthur.ss06@aluno.ifsc.edu.br>
2022-09-30 20:25:20 +0000

    implementado sistema de derrota e ajuda

--- chatbot2.py
+++ chatbot2.py
@@ -12,95 +12,147 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(intents=intents, command_prefix='')
 
-
 @bot.event
 async def on_ready():
     print('Bot is ready.')
-
 
 @bot.event
 async def on_message(msg):
     autor = msg.author.id
     if autor == bot.application_id:
         return
-
     if autor not in partidas:
-        partidas[autor] = 0
+        partidas[autor] = 8
         status_dos_jogadores[autor] = {
             'Sanidade': 50,
             'Popularidade': 50,
             'Notas': 50,
             'Inteligência': 50
         }
-
-
     estado_do_jogador = estados[partidas[autor]]
     status_sanidade = status_dos_jogadores[autor]['Sanidade']
     status_popularidade = status_dos_jogadores[autor]['Popularidade']
     status_notas = status_dos_jogadores[autor]['Notas']
     status_inteligencia = status_dos_jogadores[autor]['Inteligência']
-
     for key, value in estado_do_jogador['positivo_proximos_estados'].items():
-
-        if fullmatch(key, msg.content):
-            
+        if fullmatch(key, msg.content):            
             await msg.channel.send(estado_do_jogador['frases_positivas'])
-
             status_dos_jogadores[autor]['Sanidade'] = status_sanidade + int(estado_do_jogador['positivos_sanidade'])
             status_dos_jogadores[autor]['Popularidade'] = status_popularidade + int(estado_do_jogador['positivos_popularidade'])
             status_dos_jogadores[autor]['Notas'] = status_notas + int(estado_do_jogador['positivos_notas'])
-            status_dos_jogadores[autor]['Inteligência'] = status_inteligencia + int(estado_do_jogador['positivos_inteligência'])
-
-            await msg.channel.send(status_dos_jogadores[autor])
-            
-            if value != 8000: #usar um diferente de 0 aqui para ser mais facil
-                partidas[autor] = value
-                estado_do_jogador = estados[partidas[autor]]
+            status_dos_jogadores[autor]['Inteligência'] = status_inteligencia + int(estado_do_jogador['positivos_inteligência'])            
+            if partidas[autor] == 0 or partidas[autor] == 1 or partidas[autor] == 2 or partidas[autor] == 3 or partidas[autor] == 4 or partidas[autor] == 5 or partidas[autor] == 6 or partidas[autor] == 7:
+                status_dos_jogadores[autor]['Sanidade'] = 50
+                status_dos_jogadores[autor]['Popularidade'] = 50
+                status_dos_jogadores[autor]['Notas'] = 50
+                status_dos_jogadores[autor]['Inteligência'] = 50
+                if value != 8000:
+                    partidas[autor] = value
+                    estado_do_jogador = estados[partidas[autor]]
+                else:
+                    estado_anterior = partidas[autor]
+                    moeda = estado_anterior
+                    while estado_anterior == moeda:
+                        moeda = random.randint(8, 9)
+                    partidas[autor] = moeda
+                    flip = random.randint(15, 16)#fazer ficar 1 à 25
+                    if flip == 17:
+                        partidas[autor] = random.randint(2048, 2051)
+            elif status_dos_jogadores[autor]['Sanidade'] <= 0:
+                partidas[autor] = 0                
+            elif status_dos_jogadores[autor]['Sanidade'] >= 100:
+                partidas[autor] = 1                
+            elif status_dos_jogadores[autor]['Popularidade'] <= 0:
+                partidas[autor] = 2                
+            elif status_dos_jogadores[autor]['Popularidade'] >= 100:
+                partidas[autor] = 3               
+            elif status_dos_jogadores[autor]['Notas'] <= 0:
+                partidas[autor] = 4                
+            elif status_dos_jogadores[autor]['Notas'] >= 100:
+                partidas[autor] = 5                
+            elif status_dos_jogadores[autor]['Inteligência'] <= 0:
+                partidas[autor] = 6                
+            elif status_dos_jogadores[autor]['Inteligência'] >= 100:
+                partidas[autor] = 7
             else:
-                estado_anterior = partidas[autor]
-                moeda = estado_anterior
-                while estado_anterior == moeda:
-                    moeda = random.randint(0, 1)
-                partidas[autor] = moeda
-                flip = random.randint(15, 16)#fazer ficar 1 à 25
-                if flip == 17:
-                    partidas[autor] = random.randint(2048, 2051)
+                await msg.channel.send(status_dos_jogadores[autor])            
+                if value != 8000:
+                    partidas[autor] = value
+                    estado_do_jogador = estados[partidas[autor]]
+                else:
+                    estado_anterior = partidas[autor]
+                    moeda = estado_anterior
+                    while estado_anterior == moeda:
+                        moeda = random.randint(8, 9)
+                    partidas[autor] = moeda
+                    flip = random.randint(15, 16)#fazer ficar 1 à 25
+                    if flip == 17:
+                        partidas[autor] = random.randint(2048, 2051)
             estado_do_jogador = estados[partidas[autor]]
-            await msg.channel.send(partidas[autor])
             await msg.channel.send(choice(estado_do_jogador['frases']))
             return
     for key, value in estado_do_jogador['negativa_proximos_estados'].items():
-        if fullmatch(key, msg.content):
-            
+        if fullmatch(key, msg.content):            
             await msg.channel.send(estado_do_jogador['frases_negativas'])
-
             status_dos_jogadores[autor]['Sanidade'] = status_sanidade + int(estado_do_jogador['negativos_sanidade'])
             status_dos_jogadores[autor]['Popularidade'] = status_popularidade + int(estado_do_jogador['negativos_popularidade'])
             status_dos_jogadores[autor]['Notas'] = status_notas + int(estado_do_jogador['negativos_notas'])
             status_dos_jogadores[autor]['Inteligência'] = status_inteligencia + int(estado_do_jogador['negativos_inteligência'])
-
-            await msg.channel.send(status_dos_jogadores[autor])
-            
-            if value != 8000: #usar um diferente de 0 aqui para ser mais facil
-                partidas[autor] = value
-                estado_do_jogador = estados[partidas[autor]]
+            if partidas[autor] == 0 or partidas[autor] == 1 or partidas[autor] == 2 or partidas[autor] == 3 or partidas[autor] == 4 or partidas[autor] == 5 or partidas[autor] == 6 or partidas[autor] == 7:
+                status_dos_jogadores[autor]['Sanidade'] = 50
+                status_dos_jogadores[autor]['Popularidade'] = 50
+                status_dos_jogadores[autor]['Notas'] = 50
+                status_dos_jogadores[autor]['Inteligência'] = 50
+                if value != 8000:
+                    partidas[autor] = value
+                    estado_do_jogador = estados[partidas[autor]]
+                else:
+                    estado_anterior = partidas[autor]
+                    moeda = estado_anterior
+                    while estado_anterior == moeda:
+                        moeda = random.randint(8, 9)
+                    partidas[autor] = moeda
+                    flip = random.randint(15, 16)#fazer ficar 1 à 25
+                    if flip == 17:
+                        partidas[autor] = random.randint(2048, 2051)
+            elif status_dos_jogadores[autor]['Sanidade'] <= 0:
+                partidas[autor] = 0                
+            elif status_dos_jogadores[autor]['Sanidade'] >= 100:
+                partidas[autor] = 1                
+            elif status_dos_jogadores[autor]['Popularidade'] <= 0:
+                partidas[autor] = 2                
+            elif status_dos_jogadores[autor]['Popularidade'] >= 100:
+                partidas[autor] = 3                
+            elif status_dos_jogadores[autor]['Notas'] <= 0:
+                partidas[autor] = 4                
+            elif status_dos_jogadores[autor]['Notas'] >= 100:
+                partidas[autor] = 5               
+            elif status_dos_jogadores[autor]['Inteligência'] <= 0:
+                partidas[autor] = 6                
+            elif status_dos_jogadores[autor]['Inteligência'] >= 100:
+                partidas[autor] = 7
             else:
-                estado_anterior = partidas[autor]
-                moeda = estado_anterior
-                while estado_anterior == moeda:
-                    moeda = random.randint(0, 1)
-                partidas[autor] = moeda
-                flip = random.randint(15, 16)#fazer ficar 1 à 25
-                if flip == 17:
-                    partidas[autor] = random.randint(2048, 2051)
+                await msg.channel.send(status_dos_jogadores[autor])            
+                if value != 8000:
+                    partidas[autor] = value
+                    estado_do_jogador = estados[partidas[autor]]
+                else:
+                    estado_anterior = partidas[autor]
+                    moeda = estado_anterior
+                    while estado_anterior == moeda:
+                        moeda = random.randint(8, 9)
+                    partidas[autor] = moeda
+                    flip = random.randint(15, 16)#fazer ficar 1 à 25
+                    if flip == 17:
+                        partidas[autor] = random.randint(2048, 2051)
             estado_do_jogador = estados[partidas[autor]]
-            await msg.channel.send(partidas[autor])
             await msg.channel.send(choice(estado_do_jogador['frases']))
             return
-    if partidas[autor] == 0:
+    if partidas[autor] >= 8 and msg.content.startswith('$socorro'):
         await msg.channel.send(choice(estado_do_jogador['frases']))
-    else:
-        await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
+        await msg.channel.send(status_dos_jogadores[autor])
+    elif partidas[autor] >= 8 and msg.content.startswith('$'):
+        await msg.channel.send('Digite \$socorro para receber ajuda')
 
 
 bot.run(getenv('DISCORD_TOKEN'))
--- definições.py
+++ definições.py
@@ -2,43 +2,151 @@
 # As opções dos jogadores são definidas como expressões regulares.
 estados = {
     0: {
-        'frases': ['Ricardito quer parte do teu lanche, você dá\? estado0'],
-        'frases_positivas': 'isso não é muito legal',
-        'frases_negativas': 'descisão certa, ganha respeito',
-        'positivos_sanidade': -30, 
-        'positivos_popularidade': 20,
+        'frases': ['morte sanidade baixa estado0 suicidio'],
+        'frases_positivas': 'recomeçando jogo',
+        'frases_negativas': 'recomeçando jogo',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
         'positivos_notas': 0,
         'positivos_inteligência': 0,
-        'negativos_sanidade': 30, 
+        'negativos_sanidade': 0, 
         'negativos_popularidade': 0,
         'negativos_notas': 0,
         'negativos_inteligência': 0,
         'positivo_proximos_estados': {
-            '\$[sS](i)+m': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
         'negativa_proximos_estados': {
-            '\$[nN][aã]+o': 7
+            '\$[oO] que': 8000
         }             
     },
     1: {
-        'frases': ['estado1'],
+        'frases': ['morte sanidade alta estado1'],
         'frases_positivas': 'isso não é muito legal',
         'frases_negativas': 'descisão certa, ganha respeito',
-        'positivos_sanidade': -30, 
+        'positivos_sanidade': 0, 
         'positivos_popularidade': 0,
         'positivos_notas': 0,
         'positivos_inteligência': 0,
-        'negativos_sanidade': 30, 
+        'negativos_sanidade': 0, 
         'negativos_popularidade': 0,
         'negativos_notas': 0,
         'negativos_inteligência': 0,
         'positivo_proximos_estados': {
-            '\$[sS](i)+m': 8000},
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
         'negativa_proximos_estados': {
-            '\$[nN][aã]+o': 2
-        }
+            '\$[oO] que': 8000
+        }             
     },
     2: {
-        'frases': ['estado2'],
+        'frases': ['morte popularidade baixa estado2'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': -30, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 30, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    3: {
+        'frases': ['morte popularidade alta estado3'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 0, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    4: {
+        'frases': ['morte notas baixa estado4'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 0, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    5: {
+        'frases': ['morte notas alta estado5'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 0, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    6: {
+        'frases': ['morte inteligência baixa estado6'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 0, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    7: {
+        'frases': ['morte inteligência alta estado7'],
+        'frases_positivas': 'isso não é muito legal',
+        'frases_negativas': 'descisão certa, ganha respeito',
+        'positivos_sanidade': 0, 
+        'positivos_popularidade': 0,
+        'positivos_notas': 0,
+        'positivos_inteligência': 0,
+        'negativos_sanidade': 0, 
+        'negativos_popularidade': 0,
+        'negativos_notas': 0,
+        'negativos_inteligência': 0,
+        'positivo_proximos_estados': {
+            '\$[oO] que': 8000},               #usar 8000 para o proximo cenario ser aleatorio
+        'negativa_proximos_estados': {
+            '\$[oO] que': 8000
+        }             
+    },
+    8: {
+        'frases': ['estado8'],
         'frases_positivas': 'isso não é muito legal',
         'frases_negativas': 'descisão certa, ganha respeito',
         'positivos_sanidade': -30, 
@@ -52,11 +160,12 @@ estados = {
         'positivo_proximos_estados': {
             '\$[sS](i)+m': 8000},
         'negativa_proximos_estados': {
-            '\$[nN][aã]+o': 2
+            '\$[nN][aã]+o': 8000
         }
+        
     },
-    3: {
-        'frases': ['estado3'],
+    9: {
+        'frases': ['estado9'],
         'frases_positivas': 'isso não é muito legal',
         'frases_negativas': 'descisão certa, ganha respeito',
         'positivos_sanidade': -30, 
@@ -68,28 +177,9 @@ estados = {
         'negativos_notas': 0,
         'negativos_inteligência': 0,
         'positivo_proximos_estados': {
-            '\$[sS](i)+m': 7},
+            '\$[sS](i)+m': 8000},
         'negativa_proximos_estados': {
-            '\$[nN][aã]+o': 2
-        }
-
-    },
-    7: {
-        'frases': ['estado7'],
-        'frases_positivas': 'isso não é muito legal',
-        'frases_negativas': 'descisão certa, ganha respeito',
-        'positivos_sanidade': -30, 
-        'positivos_popularidade': 0,
-        'positivos_notas': 0,
-        'positivos_inteligência': 0,
-        'negativos_sanidade': 30, 
-        'negativos_popularidade': 0,
-        'negativos_notas': 0,
-        'negativos_inteligência': 0,
-        'positivo_proximos_estados': {
-            '\$[sS](i)+m': 7},
-        'negativa_proximos_estados': {
-            '\$[nN][aã]+o': 2
+            '\$[nN][aã]+o': 8000
         }
         
     },
