ee54079f2bcadb5a5042e4448a61792d066ecd6a
ArthurSchSilva-Schneider <arthur.ss06@aluno.ifsc.edu.br>
2022-10-01 00:39:40 +0000

    arrumado problema de espaçamento

--- teste2.py
+++ teste2.py
@@ -15,32 +15,32 @@ sorteio = random.choice(senarios)
 print('Você quer participar da aula do Carlyle?')
 
 while sanidade >= 0 and sanidade <= 100 and sanidade != 0 and sanidade != 100 and popularidade >= 0 and popularidade <= 100 and popularidade != 0 and popularidade != 100 and notas >= 0 and notas <= 100 and notas != 0 and notas != 100 and inteligência >= 0 and inteligência <= 100 and inteligência != 0 and inteligência != 100:
-    resposta = input("Sua descisão: ")
-    if resposta.startswith('sim'):
-            sanidade = sanidade
-            popularidade = popularidade
-            notas = notas
-            inteligência = inteligência
-            print('Sanidade: ' + str(sanidade))
-            print('Popularidade: ' + str(popularidade))
-            print('Notas: ' + str(notas))
-            print('Inteligência: ' + str(inteligência))
-            senarios.append('S1')
-    elif resposta.startswith('nao'):
-            sanidade = sanidade
-            popularidade = popularidade + 10
-            notas = notas
-            inteligência = inteligência
-            print('Sanidade: ' + str(sanidade))
-            print('Popularidade: ' + str(popularidade))
-            print('Notas: ' + str(notas))
-            print('Inteligência: ' + str(inteligência))
-    else:
-        print('digita direito capeta')
-        print('Sanidade: ' + str(sanidade))
-        print('Popularidade: ' + str(popularidade))
-        print('Notas: ' + str(notas))
-        print('Inteligência: ' + str(inteligência))
+        resposta = input("Sua descisão: ")
+        if resposta.startswith('sim'):
+                sanidade = sanidade
+                popularidade = popularidade
+                notas = notas
+                inteligência = inteligência
+                print('Sanidade: ' + str(sanidade))
+                print('Popularidade: ' + str(popularidade))
+                print('Notas: ' + str(notas))
+                print('Inteligência: ' + str(inteligência))
+                senarios.append('S1')
+        elif resposta.startswith('nao'):
+                sanidade = sanidade
+                popularidade = popularidade + 10
+                notas = notas
+                inteligência = inteligência
+                print('Sanidade: ' + str(sanidade))
+                print('Popularidade: ' + str(popularidade))
+                print('Notas: ' + str(notas))
+                print('Inteligência: ' + str(inteligência))
+        else:
+                print('digita direito capeta')
+                print('Sanidade: ' + str(sanidade))
+                print('Popularidade: ' + str(popularidade))
+                print('Notas: ' + str(notas))
+                print('Inteligência: ' + str(inteligência))
 
 
 
